00cf1a9a573bb984d4ac02199d00ef4afc81a3a9
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-06-12 01:08:14 -0700

    Change testing URLS

--- res/raw/jsonomfgb.js
+++ res/raw/jsonomfgb.js
@@ -3,15 +3,17 @@
 "device"="Incredible",
 "base"="1.3.0",
 "version"=3,
+"name"="OMFGB-inc-1.3.0-3.zip",
 "date"="Some date",
-"url"="http://bit.ly/iTP7TB"
+"url"="http://bit.ly/l4tntd"
 },
 
 {
 "device"="Incredible",
 "base"="1.3.0",
 "version"=2,
+"name"="OMFGB-inc-1.3.0-2.zip",
 "date"="Some date",
-"url"="http://bit.ly/iTP7TB"
+"url"="http://r2doesinc.bitsurge.net/Addons/OMFGBk-sbc-1.zip"
 }
 ]
